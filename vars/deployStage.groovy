6fcac97f79bfca809d83127c2f971930bcfc5157
supriya16-git <supriyasoft16@gmail.com>
2025-02-05 08:09:02 +0530

    Update deployStage.groovy

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -1,8 +1,8 @@
 // vars/deployStage.groovy
 
-def call(String build_number) {
+def call(String build_number, String artifactory_ip) {
             sh 'curl ifconfig.me'
-            sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://65.2.172.21:8082/artifactory/jenkins-hello-world-libs-release/com/efsavage/hello-world-war/${build_number}/hello-world-war-${build_number}.war"'
+            sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://${artifactory_ip}:8082/artifactory/jenkins-hello-world-libs-release/com/efsavage/hello-world-war/${build_number}/hello-world-war-${build_number}.war"'
             sh 'ls'
             sh 'sudo cp hello-world-war-${build_number}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/'
             sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/shutdown.sh'
