4843b1a10d18e37c1b23523677073a74eacaf5fa
AHP1609 <anuharish67@gmail.com>
2025-02-06 19:45:58 +0530

    Update deploy_stage.groovy

--- vars/deploy_stage.groovy
+++ vars/deploy_stage.groovy
@@ -1,7 +1,7 @@
 // vars/deploy_stage.groovy
 
 def call() {
-        sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://http://13.201.19.97:8082//artifactory/helloworldwar-cicd-libs-release/com/efsavage/hello-world-war/$BUILD_NUMBER/hello-world-war-$BUILD_NUMBER.war"'
+        sh 'curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://13.201.19.97:8082//artifactory/helloworldwar-cicd-libs-release/com/efsavage/hello-world-war/$BUILD_NUMBER/hello-world-war-$BUILD_NUMBER.war"'
         sh 'ls'
         sh 'sudo cp hello-world-war-$BUILD_NUMBER.war /opt/tomcat/apache-tomcat-10.1.34/webapps/'
         sh 'sudo bash /opt/tomcat/apache-tomcat-10.1.34/bin/shutdown.sh'
