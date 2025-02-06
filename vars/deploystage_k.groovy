456d1897985262cc85131f791bdffb735ca55c3f
kevalohith <118033936+kevalohith@users.noreply.github.com>
2025-02-06 17:25:14 +0530

    Update deploystage_k.groovy

--- vars/deploystage_k.groovy
+++ vars/deploystage_k.groovy
@@ -5,7 +5,7 @@ def call(String build_number, String artifactory_ip) {
             echo " ${artifactory_ip}"
             echo "${build_number}"
             sh """
-            curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://${artifactory_ip}:8082/artifactory/jenkins-hello-world-libs-release/com/efsavage/hello-world-war/${build_number}/hello-world-war-${build_number}.war"
+            curl -L -u "${ARTIFACTORY_CREDENTIALS_USR}:${ARTIFACTORY_CREDENTIALS_PSW}" -O "http://${artifactory_ip}:8082/artifactory/jenkins-world-libs-release/com/efsavage/hello-world-war/${BUILD_NUMBER}/hello-world-war-${BUILD_NUMBER}.war"
             """
             sh """
             sudo cp hello-world-war-${build_number}.war /opt/tomcat/apache-tomcat-10.1.34/webapps/
